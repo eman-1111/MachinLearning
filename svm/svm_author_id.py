d8fcf41c5c7a1708f465a6f991800cd17a2f17f4
Eman <eman.ashour1111@gmail.com>
2018-04-25 10:41:41 +0200

    add quiz 2 solution

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,48 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn import svm
 
+
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+
+#print len(features_train)
+#print len(labels_train)
+
+#arr = features_train[0]
+#arr2 = arr[:len(arr)/5]
+#print arr2
+#print type(arr2)
+
+
+# C=10.0, accuracy_score  = 0.616040955631
+# C=100.0, accuracy_score  = 0.616040955631
+# C=1000.0, accuracy_score  = 0.821387940842
+# C=10000.0, accuracy_score  = 0.892491467577
+
+t0 = time()
+
+clf = svm.SVC(C=10000.0, kernel="rbf")
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+
+
+print acc
+print pred[50]
+
+chris = 0
+for y in pred:
+	if y == 1:
+		chris += 1
+
+print chris
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -42,15 +42,18 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
+	#in this scheme, features and samples are defined as follows:
 
+    #A corpus of documents can thus be represented by a matrix with one row per document and one column per token (e.g. word) occurring in the corpus.
     ### text vectorization--go from strings to lists of numbers
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                                  stop_words='english')
     features_train_transformed = vectorizer.fit_transform(features_train)
     features_test_transformed  = vectorizer.transform(features_test)
 
-
-
+	#Function taking two arrays X and y, and returning a pair of arrays (scores, pvalues) or a single array with scores. Default is f_classif
+    #SelectPercentile removes all but a user-specified highest scoring percentage of features
+	
     ### feature selection, because text is super high dimensional and 
     ### can be really computationally chewy as a result
     selector = SelectPercentile(f_classif, percentile=10)
@@ -58,7 +61,15 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_train_transformed = selector.transform(features_train_transformed).toarray()
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
+    #print "test1", features_train_transformed
+    #print "test2", features_test_transformed
+    #print "test3", type(features_train_transformed)
+    #print "test4", type(features_test_transformed)
+    #print "test5", labels_test
+    #print "test6", type(labels_test)
+	
     ### info on the data
+	 
     print "no. of Chris training emails:", sum(labels_train)
     print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
     
