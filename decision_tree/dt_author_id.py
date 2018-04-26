f382edeabdc4f24de116ae3187aaed2f0fe24034
eman ashour <eman.ashour1111@gmail.com>
2018-04-26 09:07:36 +0200

    add quiz 3 solution

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train,labels_train)
+
+pred = clf.predict(features_test)
+
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+
+print acc
+#What's the number of features in your data
+print len(features_train[0])
+
 
 #########################################################
 
