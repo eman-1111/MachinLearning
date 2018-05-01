4742ba9a219d217ee3be504102afd6331e9b4641
Eman <eman.ashour1111@gmail.com>
2018-05-01 13:45:19 +0200

    add quiz 6 solution

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,9 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,12 +39,20 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
 
+reg = linear_model.LinearRegression()
+reg.fit(feature_train , target_train)
 
+#What are the slope and intercept?
+print reg.coef_
+print reg.intercept_
 
+#What is the score of the regression on the training data
+print reg.score(feature_train, target_train)
 
-
-
+#What is the score of the regression on the test data
+print reg.score(feature_test, target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +74,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r") 
+
+#What is the slope of the new line?
+print reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
